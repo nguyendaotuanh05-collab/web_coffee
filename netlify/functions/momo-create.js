1ada5f51af7fe87248cc7bf44d9e1c5817c2f96c
nguyendaotuanh05-collab <nguyendaotuanh05@gmail.com>
2025-11-15 16:42:47 +0700

    Update momo-create.js

--- netlify/functions/momo-create.js
+++ netlify/functions/momo-create.js
@@ -12,7 +12,7 @@ const accessKey = 'F8BBA842ECF85';
 const secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
 const partnerCode = 'MOMO';
 
-const redirectUrl = 'https://23dh.netlify.app/api-result'; // Thay bằng URL trang web của bạn
+const redirectUrl = 'https://23dh.netlify.app/Trangchu.html';
 const ipnUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b'; 
 
 const host = "test-payment.momo.vn";
