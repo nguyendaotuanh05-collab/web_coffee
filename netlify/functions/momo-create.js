a153027fab2caa8d5edeb423d1c6fc6ad68bdcb2
nguyendaotuanh05-collab <nguyendaotuanh05@gmail.com>
2025-11-16 11:53:36 +0700

    Update momo-create.js

--- netlify/functions/momo-create.js
+++ netlify/functions/momo-create.js
@@ -8,12 +8,6 @@ const crypto = require('crypto');
 // 1. CẤU HÌNH MOMO (Sử dụng biến môi trường là cách TỐT NHẤT)
 // Hiện tại chúng ta dùng giá trị cứng để dễ test, nhưng nên chuyển sang process.env
 // -----------------------------------------------------------------------------
-// const accessKey = 'F8BBA842ECF85';
-//const secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
-// const partnerCode = 'MOMO';
-
-
-//const ipnUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b'; 
 
 // Các giá trị này PHẢI được lấy từ Netlify Environment Variables
 const accessKey = process.env.MOMO_ACCESS_KEY;
