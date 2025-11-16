0ca8ddef18c577e49a1ba16c05c146c31560b2f5
nguyendaotuanh05-collab <nguyendaotuanh05@gmail.com>
2025-11-16 11:39:29 +0700

    Update momo-create.js

--- netlify/functions/momo-create.js
+++ netlify/functions/momo-create.js
@@ -12,7 +12,7 @@ const crypto = require('crypto');
 //const secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
 // const partnerCode = 'MOMO';
 
-//const redirectUrl = 'https://23dh.netlify.app/trangchu';
+
 //const ipnUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b'; 
 
 // Các giá trị này PHẢI được lấy từ Netlify Environment Variables
